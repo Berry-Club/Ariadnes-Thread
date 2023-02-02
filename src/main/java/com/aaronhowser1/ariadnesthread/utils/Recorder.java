07667997a48df99fdc25933d9a0156f05d84429a
Aaron Howser <aaronhowser1@gmail.com>
2023-02-02 06:40:51 -0600

    Revert "stfu lat"
    This reverts commit 55ecdfcce47ea04bf8449011d3fbffdf32a4a98f.

--- src/main/java/com/aaronhowser1/ariadnesthread/utils/Recorder.java
+++ src/main/java/com/aaronhowser1/ariadnesthread/utils/Recorder.java
@@ -1,62 +1,36 @@
 package com.aaronhowser1.ariadnesthread.utils;
 
-import com.aaronhowser1.ariadnesthread.AriadnesThread;
-import net.minecraft.server.MinecraftServer;
+import com.aaronhowser1.ariadnesthread.config.ServerConfigs;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.minecraftforge.fml.loading.FMLPaths;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
+import java.util.ArrayList;
 
 public class Recorder {
 
-    private static final Path localFolder = FileUtils.ensureDirectory(FMLPaths.CONFIGDIR.get().getParent().resolve("local"));
-    private static final Path locationsFolder = FileUtils.ensureDirectory(localFolder.resolve(AriadnesThread.MOD_ID));
+    private final ArrayList<Position> positions = new ArrayList<>();
 
-    public static void recordPositions(MinecraftServer minecraftServer) {
-        minecraftServer.getPlayerList().getPlayers().forEach(Recorder::recordPosition);
-    }
-
-    private static void recordPosition(Player player) {
-        String playerName = player.getScoreboardName();
-
-        Path playerFile = FileUtils.ensureFile(locationsFolder.resolve(playerName+".txt"));
-
-        List<String> positions = FileUtils.getLines(playerFile);
-        String mostRecentPosition = positions.get(positions.size()-1);
-
-        String currentPosition = String.format(
-                "%i %i %i %s",
+    public void recordPosition(Player player) {
+        Position currentPos = new Position(
                 Mth.floor(player.getX()),
                 Mth.floor(player.getY()),
                 Mth.floor(player.getZ()),
                 player.getLevel().dimension()
         );
 
-        if (farEnough(currentPosition, mostRecentPosition)) {
-            playerFile.
+        if (farEnough(currentPos)) {
+            positions.add(currentPos);
         }
-
-//        playerFile
     }
 
-    private static boolean farEnough(String pos1, String pos2) {
-        return false;
-    }
+    private boolean farEnough(Position position) {
+        if (positions.isEmpty()) return true;
 
-//    private boolean farEnough(Position position) {
-//        if (positions.isEmpty()) return true;
-//
-//        Position mostRecent = positions.get(positions.size()-1);
-//
-//        if (position.getDimension() != mostRecent.getDimension()) return true;
-//
-//        double distance = position.toVec3().distanceTo(mostRecent.toVec3());
-//        return distance > ServerConfigs.MIN_DISTANCE.get();
-//    }
+        Position mostRecent = positions.get(positions.size()-1);
+
+        if (position.getDimension() != mostRecent.getDimension()) return true;
+
+        double distance = position.toVec3().distanceTo(mostRecent.toVec3());
+        return distance > ServerConfigs.MIN_DISTANCE.get();
+    }
 }
