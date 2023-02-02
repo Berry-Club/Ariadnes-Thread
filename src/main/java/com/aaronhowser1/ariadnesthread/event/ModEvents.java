f07bd5147027d7cd2fc2a64cc1365fbeee501fc9
Aaron Howser <aaronhowser1@gmail.com>
2023-02-02 06:40:52 -0600

    Revert "Moved to new class"
    This reverts commit e22716969b1fe7c14939adca5fa74bf4fadca034.

--- src/main/java/com/aaronhowser1/ariadnesthread/event/ModEvents.java
+++ src/main/java/com/aaronhowser1/ariadnesthread/event/ModEvents.java
@@ -1,9 +1,7 @@
 package com.aaronhowser1.ariadnesthread.event;
 
 import com.aaronhowser1.ariadnesthread.AriadnesThread;
-import com.aaronhowser1.ariadnesthread.config.ServerConfigs;
 import com.aaronhowser1.ariadnesthread.utils.ModScheduler;
-import com.aaronhowser1.ariadnesthread.utils.Recorder;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,10 +16,6 @@ public class ModEvents {
         if (event.phase == TickEvent.Phase.END) {
             tick++;
             ModScheduler.handleSyncScheduledTasks(tick);
-        }
-
-        if (tick % ServerConfigs.WAIT_TIME.get() == 0) {
-
         }
     }
 
