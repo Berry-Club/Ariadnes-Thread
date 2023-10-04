67d634502057289a8792399cabefef602e1e79eb
Aaron Howser <aaronhowser1@gmail.com>
2023-10-04 13:35:18 -0500

    Remove GL11 stuff

--- src/main/kotlin/com/aaronhowser1/ariadnesthread/client/LineSegment.kt
+++ src/main/kotlin/com/aaronhowser1/ariadnesthread/client/LineSegment.kt
@@ -3,7 +3,6 @@ package com.aaronhowser1.ariadnesthread.client
 import com.aaronhowser1.ariadnesthread.utils.Location
 import net.minecraft.world.level.Level
 import net.minecraftforge.client.event.RenderLevelStageEvent
-import org.lwjgl.opengl.GL11
 
 data class LineSegment(
     val start: Location,
@@ -18,17 +17,6 @@ data class LineSegment(
     //  - If the distance between is beyond the configured teleport distance, either don't render at all or render with like a spiral or something
 
     fun render(event: RenderLevelStageEvent) {
-
-        GL11.glPushMatrix()
-        GL11.glLineWidth(2f)
-        GL11.glColor3f(1f, 0f, 0f)
-        GL11.glBegin(GL11.GL_LINES)
-
-        GL11.glVertex3d(start.x.toDouble(), start.y.toDouble(), start.z.toDouble())
-        GL11.glVertex3d(end.x.toDouble(), end.y.toDouble(), end.z.toDouble())
-
-        GL11.glEnd()
-        GL11.glPopMatrix()
 
     }
 
