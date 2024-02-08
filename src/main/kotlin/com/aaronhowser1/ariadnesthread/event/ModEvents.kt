55502ba570db652c4e93c98ed0b4f184ef9727c5
Aaron Howser <aaronhowser1@gmail.com>
2024-02-08 16:29:11 -0600

    Why is EventBusSubscriber gone

--- src/main/kotlin/com/aaronhowser1/ariadnesthread/event/ModEvents.kt
+++ src/main/kotlin/com/aaronhowser1/ariadnesthread/event/ModEvents.kt
@@ -1,13 +1,17 @@
 package com.aaronhowser1.ariadnesthread.event
 
+import com.aaronhowser1.ariadnesthread.AriadnesThread
 import com.aaronhowser1.ariadnesthread.utils.ModScheduler.handleSyncScheduledTasks
 import net.minecraftforge.event.TickEvent
 import net.minecraftforge.event.TickEvent.ServerTickEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 
+@EventBusSubscriber(modid = AriadnesThread.MOD_ID)
 object ModEvents {
 
     var tick = 0
+
     @SubscribeEvent
     fun serverTick(event: ServerTickEvent) {
         if (event.phase == TickEvent.Phase.END) {
