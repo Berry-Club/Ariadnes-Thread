8d10c644f29aaa94ef8349be6663d9b0977c3677
Aaron Howser <aaronhowser1@gmail.com>
2024-02-08 19:15:39 -0600

    ClientConfig format

--- src/main/kotlin/com/aaronhowser1/ariadnesthread/config/ClientConfig.kt
+++ src/main/kotlin/com/aaronhowser1/ariadnesthread/config/ClientConfig.kt
@@ -1,26 +1,37 @@
 package com.aaronhowser1.ariadnesthread.config
 
 import net.minecraftforge.common.ForgeConfigSpec
+import net.minecraftforge.common.ForgeConfigSpec.ConfigValue
 
 object ClientConfig {
     private val BUILDER = ForgeConfigSpec.Builder()
 
-    val SPEC : ForgeConfigSpec
-    val COLOR_START : ForgeConfigSpec.ConfigValue<String>
-    val COLOR_END : ForgeConfigSpec.ConfigValue<String>
-    val THREAD_TRANSPARENCY : ForgeConfigSpec.ConfigValue<Float>
+    private val startColor: ConfigValue<String>
+    private val endColor: ConfigValue<String>
+    private val threadTransparency: ConfigValue<Double>
+
+    val SPEC: ForgeConfigSpec
+
+    val COLOR_START: String
+        get() = startColor.get()
+    val COLOR_END: String
+        get() = endColor.get()
+    val THREAD_TRANSPARENCY: Double
+        get() = threadTransparency.get()
 
     init {
         BUILDER.push(" Client configs for Ariadne's Thread")
-        COLOR_START = BUILDER
+
+        startColor = BUILDER
             .comment(" The RGB color at the start of the thread.")
             .define("Start Color", "#68D97A")
-        COLOR_END = BUILDER
+        endColor = BUILDER
             .comment(" The RGB color at the end of the thread.")
             .define("End Color", "#7177CC")
-        THREAD_TRANSPARENCY = BUILDER
+        threadTransparency = BUILDER
             .comment(" The transparency of the thread.")
-            .defineInRange("Thread Transparency", 0.9f, 0f, 1f, Float::class.java)
+            .defineInRange("Thread Transparency", 0.9, 0.0, 1.0)
+
         BUILDER.pop()
         SPEC = BUILDER.build()
     }
