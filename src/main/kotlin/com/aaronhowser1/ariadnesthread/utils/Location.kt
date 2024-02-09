a73978a70c25ba670151a2eb7fccafc71b403960
Aaron Howser <aaronhowser1@gmail.com>
2024-02-08 23:08:57 -0600

    Revert "Converting to ints"
    This reverts commit 2539f2f47561d34ad8bb70af5234eb62d2beebcb.

--- src/main/kotlin/com/aaronhowser1/ariadnesthread/utils/Location.kt
+++ src/main/kotlin/com/aaronhowser1/ariadnesthread/utils/Location.kt
@@ -1,46 +1,44 @@
 package com.aaronhowser1.ariadnesthread.utils
 
 import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.IntTag
+import net.minecraft.nbt.DoubleTag
 import net.minecraft.nbt.ListTag
 import net.minecraft.nbt.Tag
 import net.minecraft.world.phys.Vec3
 
 data class Location(
-    val x: Int,
-    val y: Int,
-    val z: Int
-) {
+    val vec3: Vec3
+) : Vec3(vec3.x, vec3.y, vec3.z) {
 
     constructor(compoundTag: CompoundTag) : this(
-        (compoundTag.get("blockPos") as ListTag).getInt(0),
-        (compoundTag.get("blockPos") as ListTag).getInt(1),
-        (compoundTag.get("blockPos") as ListTag).getInt(2)
+        Vec3(
+            (compoundTag.get("blockPos") as ListTag).getDouble(0),
+            (compoundTag.get("blockPos") as ListTag).getDouble(1),
+            (compoundTag.get("blockPos") as ListTag).getDouble(2)
+        )
     )
 
     override fun toString(): String {
-        return "Location($x, $y, $z)"
+        return "Location(${vec3.x}, ${vec3.y}, ${vec3.z})"
     }
 
     fun toTag(): Tag {
+
         val compoundTag = CompoundTag()
 
         val blockPosListTag = ListTag().apply {
-            add(IntTag.valueOf(x))
-            add(IntTag.valueOf(y))
-            add(IntTag.valueOf(z))
+            add(DoubleTag.valueOf(vec3.x))
+            add(DoubleTag.valueOf(vec3.y))
+            add(DoubleTag.valueOf(vec3.z))
         }
 
         compoundTag.put("blockPos", blockPosListTag)
 
         return compoundTag
-    }
 
-    fun closerThan(other: Location, distance: Int): Boolean {
-        return
     }
 
     companion object {
-        fun Vec3.toLocation(): Location = Location(x.toInt(), y.toInt(), z.toInt())
+        fun Vec3.toLocation(): Location = Location(this)
     }
 }
