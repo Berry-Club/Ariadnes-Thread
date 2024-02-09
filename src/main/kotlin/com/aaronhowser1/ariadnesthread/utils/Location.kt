2539f2f47561d34ad8bb70af5234eb62d2beebcb
Aaron Howser <aaronhowser1@gmail.com>
2024-02-08 23:08:53 -0600

    Converting to ints

--- src/main/kotlin/com/aaronhowser1/ariadnesthread/utils/Location.kt
+++ src/main/kotlin/com/aaronhowser1/ariadnesthread/utils/Location.kt
@@ -1,44 +1,46 @@
 package com.aaronhowser1.ariadnesthread.utils
 
 import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.DoubleTag
+import net.minecraft.nbt.IntTag
 import net.minecraft.nbt.ListTag
 import net.minecraft.nbt.Tag
 import net.minecraft.world.phys.Vec3
 
 data class Location(
-    val vec3: Vec3
-) : Vec3(vec3.x, vec3.y, vec3.z) {
+    val x: Int,
+    val y: Int,
+    val z: Int
+) {
 
     constructor(compoundTag: CompoundTag) : this(
-        Vec3(
-            (compoundTag.get("blockPos") as ListTag).getDouble(0),
-            (compoundTag.get("blockPos") as ListTag).getDouble(1),
-            (compoundTag.get("blockPos") as ListTag).getDouble(2)
-        )
+        (compoundTag.get("blockPos") as ListTag).getInt(0),
+        (compoundTag.get("blockPos") as ListTag).getInt(1),
+        (compoundTag.get("blockPos") as ListTag).getInt(2)
     )
 
     override fun toString(): String {
-        return "Location(${vec3.x}, ${vec3.y}, ${vec3.z})"
+        return "Location($x, $y, $z)"
     }
 
     fun toTag(): Tag {
-
         val compoundTag = CompoundTag()
 
         val blockPosListTag = ListTag().apply {
-            add(DoubleTag.valueOf(vec3.x))
-            add(DoubleTag.valueOf(vec3.y))
-            add(DoubleTag.valueOf(vec3.z))
+            add(IntTag.valueOf(x))
+            add(IntTag.valueOf(y))
+            add(IntTag.valueOf(z))
         }
 
         compoundTag.put("blockPos", blockPosListTag)
 
         return compoundTag
+    }
 
+    fun closerThan(other: Location, distance: Int): Boolean {
+        return
     }
 
     companion object {
-        fun Vec3.toLocation(): Location = Location(this)
+        fun Vec3.toLocation(): Location = Location(x.toInt(), y.toInt(), z.toInt())
     }
 }
