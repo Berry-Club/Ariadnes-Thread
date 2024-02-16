cc9d36bb1c063f5732a2e05b5024e41be57db007
Aaron Howser <aaronhowser1@gmail.com>
2024-02-16 02:58:08 -0600

    Moved ModServerEvents back to ModComonEvents, since ServerTickEvent wasn't firing in singleplayer

--- src/main/kotlin/com/aaronhowser1/ariadnesthread/event/ModCommonEvents.kt
+++ src/main/kotlin/com/aaronhowser1/ariadnesthread/event/ModCommonEvents.kt
@@ -2,19 +2,19 @@ package com.aaronhowser1.ariadnesthread.event
 
 import com.aaronhowser1.ariadnesthread.AriadnesThread
 import com.aaronhowser1.ariadnesthread.utils.ModScheduler
-import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.event.TickEvent
-import net.minecraftforge.event.TickEvent.ServerTickEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 
 @Mod.EventBusSubscriber(
-    modid = AriadnesThread.MOD_ID,
-    value = [Dist.DEDICATED_SERVER]
+    modid = AriadnesThread.MOD_ID
 )
-object ModServerEvents {
+object ModCommonEvents {
+
     @SubscribeEvent
-    fun serverTick(event: ServerTickEvent) {
+    fun serverTick(event: TickEvent) {
+        if (event.side.isClient) return
         if (event.phase == TickEvent.Phase.END) ModScheduler.tick++
     }
+
 }
