b0cd828a3a0d0fccab3336786dfa73fc04a0372d
Aaron Howser <aaronhowser1@gmail.com>
2024-02-27 14:02:36 -0600

    handleSyncScheduledTasks is now handled by tick's setter

--- src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/event/ModCommonEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/event/ModCommonEvents.kt
@@ -1,7 +1,7 @@
 package dev.aaronhowser.mods.ariadnesthread.event
 
 import dev.aaronhowser.mods.ariadnesthread.AriadnesThread
-import dev.aaronhowser.mods.ariadnesthread.utils.ModScheduler.handleSyncScheduledTasks
+import dev.aaronhowser.mods.ariadnesthread.utils.ModScheduler.tick
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.Mod
 import net.neoforged.neoforge.event.TickEvent
@@ -11,13 +11,10 @@ import net.neoforged.neoforge.event.TickEvent
 )
 object ModCommonEvents {
 
-    var tick = 0
-
     @SubscribeEvent
     fun serverTick(event: TickEvent.ServerTickEvent) {
         if (event.phase == TickEvent.Phase.END) {
             tick++
-            handleSyncScheduledTasks(tick)
         }
     }
 
--- src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/utils/ModScheduler.kt
+++ src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/utils/ModScheduler.kt
@@ -1,36 +1,45 @@
 package dev.aaronhowser.mods.ariadnesthread.utils
 
-import dev.aaronhowser.mods.ariadnesthread.event.ModCommonEvents
 import com.google.common.collect.HashMultimap
+import dev.aaronhowser.mods.ariadnesthread.AriadnesThread
+import org.apache.logging.log4j.Level
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
 object ModScheduler {
+
+    var tick = 0
+        set(value) {
+            field = value
+            handleSyncScheduledTasks(value)
+        }
+
     private var scheduler: ScheduledExecutorService? = null
     private val scheduledSyncTasks = HashMultimap.create<Int, Runnable>()
-    fun scheduleSynchronisedTask(ticks: Int, run: Runnable) {
-        scheduledSyncTasks.put(ModCommonEvents.tick + ticks, run)
+
+    private fun scheduleSynchronisedTask(ticks: Int, run: Runnable) {
+        scheduledSyncTasks.put(tick + ticks, run)
     }
 
-    fun scheduleAsyncTask(time: Int, unit: TimeUnit?, run: Runnable) {
+    private fun scheduleAsyncTask(time: Int, unit: TimeUnit?, run: Runnable) {
         if (scheduler == null) serverStartupTasks()
         scheduler!!.schedule(run, time.toLong(), unit)
     }
 
-    fun serverStartupTasks() {
+    private fun serverStartupTasks() {
         if (scheduler != null) scheduler!!.shutdownNow()
         scheduler = Executors.newScheduledThreadPool(1)
         handleSyncScheduledTasks(null)
     }
 
-    fun serverShutdownTasks() {
+    private fun serverShutdownTasks() {
         handleSyncScheduledTasks(null)
         scheduler!!.shutdownNow()
         scheduler = null
     }
 
-    fun handleSyncScheduledTasks(tick: Int?) {
+    private fun handleSyncScheduledTasks(tick: Int?) {
         if (scheduledSyncTasks.containsKey(tick)) {
             val tasks =
                 if (tick == null) scheduledSyncTasks.values().iterator() else scheduledSyncTasks[tick].iterator()
@@ -38,7 +47,7 @@ object ModScheduler {
                 try {
                     tasks.next().run()
                 } catch (ex: Exception) {
-//                    Logging.logMessage(Level.ERROR, "Unable to run unhandled scheduled task, skipping.", ex);
+                    AriadnesThread.LOGGER.log(Level.ERROR, "Error executing scheduled task", ex)
                 }
                 tasks.remove()
             }
