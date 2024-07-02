d00f242f5ef0f6c35ff6a2f5f95cdc4e36b019be
Aaron Howser <aaronhowser1@gmail.com>
2024-07-02 16:04:33 -0500

    Actually register the registries

--- src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/AriadnesThread.kt
+++ src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/AriadnesThread.kt
@@ -1,11 +1,13 @@
 package dev.aaronhowser.mods.ariadnesthread
 
 import dev.aaronhowser.mods.ariadnesthread.config.ClientConfig
+import dev.aaronhowser.mods.ariadnesthread.registry.ModRegistries
 import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.config.ModConfig
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 
 @Mod(AriadnesThread.ID)
 class AriadnesThread(
@@ -19,5 +21,7 @@ class AriadnesThread(
 
     init {
         modContainer.registerConfig(ModConfig.Type.CLIENT, ClientConfig.CONFIG_SPEC)
+
+        ModRegistries.register(MOD_BUS)
     }
 }
