9233dab7918d09a9f5caa2d2dc401c05f942baa2
Aaron Howser <aaronhowser1@gmail.com>
2024-07-02 14:53:30 -0500

    Server Config

---
+++ src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/config/ServerConfig.kt
@@ -0,0 +1,57 @@
+package dev.aaronhowser.mods.ariadnesthread.config
+
+import net.neoforged.neoforge.common.ModConfigSpec
+import org.apache.commons.lang3.tuple.Pair
+
+class ServerConfig(
+    private val builder: ModConfigSpec.Builder
+) {
+
+    companion object {
+
+        private val configPair: Pair<ServerConfig, ModConfigSpec> = ModConfigSpec.Builder().configure(::ServerConfig)
+
+        val CONFIG: ServerConfig = configPair.left
+        val CONFIG_SPEC: ModConfigSpec = configPair.right
+
+        lateinit var waitTime: ModConfigSpec.IntValue
+        lateinit var minDistance: ModConfigSpec.DoubleValue
+        lateinit var maxLocations: ModConfigSpec.IntValue
+        lateinit var maxNbtSize: ModConfigSpec.IntValue
+
+    }
+
+
+    init {
+        serverConfigs()
+
+        builder.build()
+    }
+
+    private fun serverConfigs() {
+        builder.push("Server")
+
+        waitTime = builder
+            .comment("The time in ticks to wait between checking location.")
+            .defineInRange("Check interval", 20, 1, Int.MAX_VALUE)
+
+        minDistance = builder
+            .comment("The minimum distance between points.\nIf you haven't moved more than this distance from your last point, it isn't saved.")
+            .defineInRange("Minimum Distance", 5.0, 0.0, Double.MAX_VALUE)
+
+        builder.push("Limits")
+
+        maxLocations = builder
+            .comment("The maximum number of locations to store. Set to 0 to disable limit.\nEnabling advanced tooltips will show how many locations are stored.")
+            .defineInRange("Max Locations", 5120, 0, Int.MAX_VALUE)
+
+        maxNbtSize = builder
+            .comment(" The maximum size of the NBT data for the thread item, in bits. Set to 0 to disable limit.\nEnabling advanced tooltips will show the current size.")
+            .defineInRange("Max NBT Size", 0, 0, Int.MAX_VALUE)
+
+        builder.pop()
+        builder.pop()
+    }
+
+
+}
