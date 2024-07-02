9bf6299c1f4c2a6eeeeba88242a5eae3dcb5a790
Aaron Howser <aaronhowser1@gmail.com>
2024-07-02 14:45:04 -0500

    scooping out old shit

--- src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/AriadnesThread.kt
+++ src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/AriadnesThread.kt
@@ -1,30 +1,15 @@
 package dev.aaronhowser.mods.ariadnesthread
 
-import dev.aaronhowser.mods.ariadnesthread.block.ModBlocks
-import net.minecraft.client.Minecraft
-import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.fml.common.Mod
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
-import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
-import thedarkcolour.kotlinforforge.neoforge.forge.runForDist
 
-/**
- * Main mod class. Should be an `object` declaration annotated with `@Mod`.
- * The modid should be declared in this object and should match the modId entry
- * in neoforge.mods.toml.
- *
- * An example for blocks is in the `blocks` package of this mod.
- */
 @Mod(AriadnesThread.ID)
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 object AriadnesThread {
-    const val ID = "examplemod"
+    const val ID = "ariadnesthread"
 
     // the logger for our mod
     val LOGGER: Logger = LogManager.getLogger(ID)
@@ -32,40 +17,5 @@ object AriadnesThread {
     init {
         LOGGER.log(Level.INFO, "Hello world!")
 
-        // Register the KDeferredRegister to the mod-specific event bus
-        ModBlocks.REGISTRY.register(MOD_BUS)
-
-        val obj = runForDist(
-            clientTarget = {
-                MOD_BUS.addListener(AriadnesThread::onClientSetup)
-                Minecraft.getInstance()
-            },
-            serverTarget = {
-                MOD_BUS.addListener(AriadnesThread::onServerSetup)
-                "test"
-            })
-
-        println(obj)
-    }
-
-    /**
-     * This is used for initializing client specific
-     * things such as renderers and keymaps
-     * Fired on the mod specific event bus.
-     */
-    private fun onClientSetup(event: FMLClientSetupEvent) {
-        LOGGER.log(Level.INFO, "Initializing client...")
-    }
-
-    /**
-     * Fired on the global Forge bus.
-     */
-    private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
-        LOGGER.log(Level.INFO, "Server starting...")
-    }
-
-    @SubscribeEvent
-    fun onCommonSetup(event: FMLCommonSetupEvent) {
-        LOGGER.log(Level.INFO, "Hello! This is working!")
     }
 }
