301f1de0db60e76902b61704e32d7553a429da0d
Aaron Howser <aaronhowser1@gmail.com>
2024-07-02 17:45:50 -0500

    Progress

--- src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/event/ClientEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/event/ClientEvents.kt
@@ -1,17 +1,18 @@
 package dev.aaronhowser.mods.ariadnesthread.event
 
 import dev.aaronhowser.mods.ariadnesthread.AriadnesThread
+import dev.aaronhowser.mods.ariadnesthread.client.ModRenderer
 import dev.aaronhowser.mods.ariadnesthread.item.ThreadItem
 import dev.aaronhowser.mods.ariadnesthread.util.ClientUtil
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.client.event.ClientTickEvent
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent
 
 @EventBusSubscriber(
-    modid = AriadnesThread.ID,
-    bus = EventBusSubscriber.Bus.MOD
+    modid = AriadnesThread.ID
 )
-object ClientModBusEvents {
+object ClientEvents {
 
     @SubscribeEvent
     fun clientTick(event: ClientTickEvent.Post) {
@@ -26,6 +27,18 @@ object ClientModBusEvents {
             ThreadItem.inStartingDimension(it, level)
         }
 
+        val histories = threadItems.map { ThreadItem.getHistory(it) }
+
+        ModRenderer.histories = histories
+    }
+
+    @SubscribeEvent
+    fun onWorldRenderLast(event: RenderLevelStageEvent) {
+        if (event.stage != RenderLevelStageEvent.Stage.AFTER_WEATHER) return
+
+        if (ClientUtil.localPlayer != null) {
+            ModRenderer.renderLines(event)
+        }
     }
 
 }
