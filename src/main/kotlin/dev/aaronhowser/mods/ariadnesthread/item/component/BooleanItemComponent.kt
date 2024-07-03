6e1bf3bff53ed96aa25faeec918754690464b6f7
Aaron Howser <aaronhowser1@gmail.com>
2024-07-03 11:29:47 -0500

    Improved the codec for boolean item component

--- src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/item/component/BooleanItemComponent.kt
+++ src/main/kotlin/dev/aaronhowser/mods/ariadnesthread/item/component/BooleanItemComponent.kt
@@ -1,10 +1,9 @@
 package dev.aaronhowser.mods.ariadnesthread.item.component
 
 import com.mojang.serialization.Codec
-import com.mojang.serialization.codecs.RecordCodecBuilder
 import dev.aaronhowser.mods.ariadnesthread.registry.ModDataComponents
+import io.netty.buffer.ByteBuf
 import net.minecraft.core.component.DataComponentType
-import net.minecraft.network.RegistryFriendlyByteBuf
 import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
 
@@ -13,16 +12,11 @@ data class BooleanItemComponent(
 ) {
     companion object {
 
-        val CODEC: Codec<BooleanItemComponent> = RecordCodecBuilder.create { instance ->
-            instance.group(
-                Codec.BOOL.fieldOf("value").forGetter(BooleanItemComponent::value)
-            ).apply(instance, ::BooleanItemComponent)
-        }
+        val CODEC: Codec<BooleanItemComponent> =
+            Codec.BOOL.xmap(::BooleanItemComponent, BooleanItemComponent::value)
 
-        val STREAM_CODEC: StreamCodec<RegistryFriendlyByteBuf, BooleanItemComponent> = StreamCodec.composite(
-            ByteBufCodecs.BOOL, BooleanItemComponent::value,
-            ::BooleanItemComponent
-        )
+        val STREAM_CODEC: StreamCodec<ByteBuf, BooleanItemComponent> =
+            ByteBufCodecs.BOOL.map(::BooleanItemComponent, BooleanItemComponent::value)
 
         val isRecordingComponent: DataComponentType<BooleanItemComponent> by lazy {
             ModDataComponents.IS_RECORDING_COMPONENT.get()
